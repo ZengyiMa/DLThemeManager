6cd06a20a2a7887a9e880ac4ff0262c8e9eade8b
mazengyi <semazengyi@gmail.com>
2016-12-09 17:06:38 +0800

    add UIKit header

---
+++ DLThemeManager/DLThemeUIKit.h
@@ -0,0 +1,21 @@
+//
+//  DLThemeUIKit.h
+//  Niupu_SNS
+//
+//  Created by famulei on 09/12/2016.
+//  Copyright © 2016 WE. All rights reserved.
+//
+
+#ifndef DLThemeUIKit_h
+#define DLThemeUIKit_h
+
+#import "NSObject+DLTheme.h"
+#import "UIButton+DLTheme.h"
+#import "UIColor+DLTheme.h"
+#import "UIImageView+DLTheme.h"
+#import "UILabel+DLTheme.h"
+#import "UIView+DLTheme.h"
+#import "UITextField+DLTheme.h"
+
+
+#endif /* DLThemeUIKit_h */
